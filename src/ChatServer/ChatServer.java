df75d9b99d978c539ccc54f0d65bc8a02bf380db
Nick Burrell <celloburrell@gmail.com>
2020-06-03 16:04:25 -0500

    Initial commit: basic classes for a chat program

---
+++ src/ChatServer/ChatServer.java
@@ -0,0 +1,10 @@
+package ChatServer;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ChatServer {
+    private int port;
+    private Set<String> userNames = new HashSet<>();
+    private Set<UserThread> userThreads = new HashSet<>();
+}
---
+++ src/ChatServer/UserThread.java
@@ -0,0 +1,4 @@
+package ChatServer;
+
+public class UserThread {
+}
